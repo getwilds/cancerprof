0b41cb50221accb263eb0c1bf7a95842b3f261c0
realbp <realbp@uw.edu>
2024-01-11 16:03:29 -0800

    added pullEducation

--- pullEducation.R
+++ pullEducation.R
@@ -2,6 +2,8 @@ library(httr2)
 library(tidyverse)
 library(dplyr)
 library(cdlTools)
+library(cli)
+
 
 req <- request("https://statecancerprofiles.cancer.gov/demographics/index.php")
 
@@ -44,22 +46,80 @@ handle_race <- function(race) {
 }
 
 
+handle_education <-function(education) {
+  education <- tolower(education)
+  
+  edu_mapping <- c( 
+    "less than 9th grade" = "00004",
+    "at least high school" = "00109",
+    "at least bachelors degree" = "00006"
+  )
+  
+  edu_code <- edu_mapping[education]
+  
+  if (is.null(edu_code)) {
+    stop("Invalid input")
+  }
+  
+  return(as.character(edi_code))
+}
 
-demo_crowding <- function(area, areatype, race) {
-  resp <- req %>% 
+
+handle_sex <-function(sex) {
+  sex <- tolower(sex)
+  
+  sex_mapping <- c( 
+    "both sexes" = "0",
+    "males" = "1",
+    "females" = "2"
+  )
+  
+  sex_code <- sex_mapping[sex]
+  
+  if (is.null(sex_code)) {
+    stop("Invalid input")
+  }
+  
+  return(as.character(sex_code))
+}
+
+
+demo_education <- function(area, areatype, education, race=NULL, sex=NULL) {
+  
+  if(education == "less than 9th grade" && (!is.null(race) || !is.null(sex))) {
+    cli_abort("For Less than 9th Grade, Race and Sex must be NULL.")
+  } else if (education == "at least high school" && !is.null(race) && is.null(sex)) {
+    cli_abort("For At Least High School, Race must be NULL and Sex must be NOT NULL.")
+  } else if (education == "at least bachelors degree" && (is.null(race) || is.null(sex))) {
+    cli_abort("For At Least Bachelors Degree, Race and Sex must be NOT NULL.")
+  }
+  
+  
+  req_draft <- req %>% 
     req_url_query(
       stateFIPS=fips(area),
       areatype=tolower(areatype),
-      topic="crowd",
-      demo="00027",
-      race=handle_race(race),
+      topic="ed",
+      demo=handle_education(education),
+      
       type="manyareacensus",
       sortVariableName="value",
       sortOrder="default",
       output=1
-    ) %>% 
-    req_perform()
+    )
+    
+    if(!is.null(race)) {
+      req_draft <- req_draft %>% 
+        req_url_query(race=handle_race(race))
+    }
+    
+    if(!is.null(sex)) {
+      req_draft <- req_draft %>% 
+        req_url_query(sex=handle_sex(sex))
+    }
   
+    resp <- req_draft %>% 
+      req_perform() 
   
   resp_lines <- resp %>% 
     resp_body_string() %>% 
@@ -77,4 +137,6 @@ demo_crowding <- function(area, areatype, race) {
 }
 
 
-demo_crowding("WA", "county", "black")
+demo_education("wa", "county", "at least high school", sex="males")
+
+
