8f3a3f8b8fe3c0cca178b1758e28a3889d0cb219
realbp <realbp@uw.edu>
2024-01-17 11:17:48 -0800

    created a process response function

---
+++ process-response.R
@@ -0,0 +1,98 @@
+#' Process Response Data
+#' 
+#' This function returns a data frame from Crowding in State Cancer Profiles
+#'
+#' @param area A state/territory abbreviation or USA.
+#' @param areatype Either "county" or "HSA" (Health service area)
+#' @param race One of the following values: "All Races (includes Hispanic)", "white (includes hispanic)" = "01",
+#'              "white non-hispanic","black","amer. indian/alaskan native (includes hispanic)",
+#'              "asian or pacific islander (includes hispanic)","hispanic (any race)
+#' 
+#' specify new line for each package
+#' @importFrom httr2 req_url_query, req_perform, resp_body_string
+#' @importFrom cdlTools fips
+#' 
+#' @returns A data frame with the following columns "County", "FIPS", "Percent", "Households", "Rank"
+#' 
+#' @export
+#' 
+#' @examples 
+#' demo_crowding("WA", "county", "All Races (includes Hispanic)")
+#' 
+
+
+req <- create_request()
+
+resp <- req %>% 
+  req_url_query(
+    stateFIPS="53",
+    areatype="county",
+    topic="crowd",
+    demo="00027",
+    race="02",
+    type="manyareacensus",
+    sortVariableName="value",
+    sortOrder="default",
+    output=1
+  ) %>% 
+  req_perform()
+
+resp
+
+
+
+process_response <- function(resp) {
+  resp_lines <- resp %>% 
+    resp_body_string() %>% 
+    strsplit("\\n") %>%  unlist() 
+  
+  index_first_line_break <- which(resp_lines == "")[1]
+  index_second_line_break <- which(resp_lines == "")[2]
+  
+  resp <- resp_lines[(index_first_line_break + 1):(index_second_line_break -1)] %>% 
+    paste(collapse = "\n") %>% 
+    (\(x) read.csv(textConnection(x), header=TRUE, colClasses = "character"))() %>% 
+    filter(str_detect(County, "County")) %>% 
+    mutate_all(\(x) na_if(na_if(x, "data not available"), "N/A"))
+}
+
+# resp_lines <- resp %>% 
+#   resp_body_string() %>% 
+#   strsplit("\\n") %>%  unlist() 
+# 
+# index_first_line_break <- which(resp_lines == "")[1]
+# index_second_line_break <- which(resp_lines == "")[2]
+# 
+# resp <- resp_lines[(index_first_line_break + 1):(index_second_line_break -1)] %>% 
+#   paste(collapse = "\n") %>% 
+#   (\(x) read.csv(textConnection(x), header=TRUE, colClasses = "character"))() %>% 
+#   filter(str_detect(County, "County")) %>% 
+#   mutate_all(\(x) na_if(na_if(x, "data not available"), "N/A"))
+# 
+# 
+# 
+# 
+# resp %>% 
+#   setNames(c("County", "FIPS", "Percent", "Households", "Rank"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
