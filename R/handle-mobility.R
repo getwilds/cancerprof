9ffdfd2247a1023749cbecb28fbf5a3349946ff7
realbp <realbp@uw.edu>
2024-01-19 14:24:34 -0800

    modified mobility

---
+++ R/handle-mobility.R
@@ -0,0 +1,35 @@
+#' Handles Mobility Values to Code
+#' 
+#' This function returns a matching code value for a mobility for the api to use to get data from State Cancer Profiles
+#'
+#' @param sex Either "i haven't moved (in past year)", "moved from outside us (in past year)",
+#'            "moved, different state (in past year)", "moved, different county, same state (in past year)",
+#'            "moved, same county (in past year)"
+#' 
+#' @returns A string for its respective mobility
+#' 
+#' \dontrun{
+#' @examples 
+#' handle_mobility("under 19 years")
+#' handle_mobility(mobility)
+#' }
+handle_mobility <-function(mobility) {
+  
+  mobility <- tolower(mobility)
+  
+  mobility_mapping <- c( 
+    "i haven't moved (in past year)" = "00017",
+    "moved from outside us (in past year)" = "00021",
+    "moved, different state (in past year)" = "00020",
+    "moved, different county, same state (in past year)" = "00019",
+    "moved, same county (in past year)" = "00018"
+  )
+  
+  mobility_code <- mobility_mapping[mobility]
+  
+  if (is.null(mobility_code)) {
+    stop("Invalid input")
+  }
+  
+  return(as.character(mobility_code))
+}
--- R/mobility.R
+++ R/mobility.R
@@ -1,11 +1,3 @@
-library(httr2)
-library(tidyverse)
-library(dplyr)
-library(cdlTools)
-
-req <- request("https://statecancerprofiles.cancer.gov/demographics/index.php")
-
-
 #' Access to Mobility Data
 #' 
 #' This function returns a data frame from mobility in State Cancer Profiles
@@ -20,37 +12,15 @@ req <- request("https://statecancerprofiles.cancer.gov/demographics/index.php")
 #' 
 #' \dontrun{
 #' @examples 
-#' demo_mobility("WA", "county", "moved, same county (in past year)")
+#' demo_mobility("WA", "county", "moved, different county, same state (in past year)")
 #' }
-
-
-
-handle_mobility <-function(mobility) {
-  mobility <- tolower(mobility)
-  
-  mobility_mapping <- c( 
-    "i haven't moved (in past year)" = "00017",
-    "moved from outside us (in past year)" = "00021",
-    "moved, different state (in past year)" = "00020",
-    "moved, different county, same state (in past year)" = "00019",
-    "moved, same county (in past year)" = "00018"
-  )
-  
-  mobility_code <- mobility_mapping[mobility]
-  
-  if (is.null(mobility_code)) {
-    stop("Invalid input")
-  }
-  
-  return(as.character(mobility_code))
-}
-
-
-
 demo_mobility <- function(area, areatype, mobility) {
+  
+  req <- create_request("demographics")
+  
   resp <- req %>% 
     req_url_query(
-      stateFIPS=fips(area),
+      stateFIPS=fips_scp(area),
       areatype=tolower(areatype),
       topic="mob",
       demo=handle_mobility(mobility),
@@ -60,22 +30,20 @@ demo_mobility <- function(area, areatype, mobility) {
       output=1
     ) %>% 
     req_perform()
-  
-  
-  resp_lines <- resp %>% 
-    resp_body_string() %>% 
-    strsplit("\\n") %>%  unlist() 
-  
-  index_first_line_break <- which(resp_lines == "")[1]
-  index_second_line_break <- which(resp_lines == "")[2]
-  
-  resp_lines[(index_first_line_break + 1):(index_second_line_break -1)] %>% 
-    paste(collapse = "\n") %>% 
-    (\(x) read.csv(textConnection(x), header=TRUE))() %>% 
-    setNames(c("County", "FIPS", "Percent", "People", "Rank")) %>% 
-    filter(str_detect(County, "County")) 
 
+    resp <- process_response(resp)
+    
+    if (areatype == "county") {
+      resp %>% 
+        setNames(c("County", "FIPS", "Percent", "People", "Rank")) 
+    } else if (areatype == "hsa") {
+      resp %>% 
+        setNames(c("Health Service Area", "FIPS", "Percent", "People", "Rank"))
+    } else if (areatype == "state") {
+      resp %>% 
+        setNames(c("State", "FIPS", "Percent", "People", "Rank"))
+    }
 }
 
-
 demo_mobility("WA", "county", "moved, different county, same state (in past year)")
+demo_mobility("us", "hsa", "moved, same county (in past year)") ##???? Whats going on here
