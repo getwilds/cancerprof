e5979476ccb5a6cc6218d862aa6804ff02cab3e3
realbp <realbp@uw.edu>
2024-01-30 15:23:43 -0800

    modified cancer functions

--- R/incident-cancer.R
+++ R/incident-cancer.R
@@ -4,7 +4,11 @@
 #'
 #' @param area A state/territory abbreviation or USA.
 #' @param areatype Either "county", "hsa" (Health service area), or "state"
-#' @param cancer description
+#' @param cancer Either "all cancer sites","bladder", "brain & ons", "breast (female)", "breast (female in situ)", "cervix", 
+#'                      "childhood (ages <15, all sites)", "childhood (ages <20, all sites)", "colon & rectum", "esophagus", 
+#'                      "kidney & renal pelvis", "leukemia", "liver & bile duct", "lung & bronchus", "melanoma of the skin", 
+#'                      "non-hodgkin lymphoma", "oral cavity & pharynx", "ovary", "pancreas", "prostate", "stomach", 
+#'                      "thyroid", "uterus (corpus & uterus, nos)"
 #' @param race One of the following values: "all races (includes hispanic)", "white (non-hispanic)", 
 #'                                          "black (non-hispanic)", "amer. indian / ak native (non-hispanic)", 
 #'                                          "asian / pacific islander (non-hispanic)","hispanic (any race)"
@@ -18,21 +22,38 @@
 #' 
 #' @examples
 #' \dontrun{
-#' incidence_cancer("percent who received 2+ doses of HPV vaccine, ages 13-15", "both sexes")
+#' incidence_cancer("wa", "county", "lung & bronchus", "all races (includes hispanic)", "males", "ages 50+", "late stage (regional & distant)", "latest 5 year average")
+#' incidence_cancer("usa", "state", "lung & bronchus", "all races (includes hispanic)", "males", "ages 50+", "late stage (regional & distant)", "latest 5 year average")
 #' incidence_cancer("percent who received 2+ doses of HPV vaccine, ages 13-17", "both sexes")
 #' incidence_cancer("percent who received 3+ doses of HPV vaccine, ages 13-17", "females")
 #' }
+
+area = "wa"
+areatype = "hsa"
+race = "all races (includes hispanic)"
+sex = "males"
+cancer = "lung & bronchus"
+age = "ages 50+"
+stage = "late stage (regional & distant)"
+year = "latest 5 year average"
+
+
 incidence_cancer <- function(area, areatype, cancer, race, sex, age, stage, year) {
   
   req <- create_request("incidencerates")
   
   resp <- req %>%
     req_url_query(
-      topic="vaccine",
-      risk=handle_vaccine(vaccine),
+      stateFIPS=fips_scp(area),
+      areatype=tolower(areatype),
+      cancer=handle_cancer(cancer),
+      race=handle_race(race),
       sex=handle_sex(sex),
-      type="risk",
-      sortVariableName="percent",
+      age=handle_age(age),
+      stage=handle_stage(stage),
+      year=handle_year(year),
+      type="incd",
+      sortVariableName="rate",
       sortOrder="default",
       output=1
     )
@@ -40,22 +61,17 @@ incidence_cancer <- function(area, areatype, cancer, race, sex, age, stage, year
   resp <- resp %>%
     req_perform()
   
-  resp <- process_screening(resp)
+  resp <- process_incidence(resp)
   
   
-  vaccine_type1 = c("percent who received 2+ doses of HPV vaccine, ages 13-15",
-                    "percent who received 3+ doses of HPV vaccine, ages 13-15"
-  )
+  areatype_map <- c("county" = "County", "hsa" = "Health Service Area", "state" = "State")
+  areatype_title <- areatype_map[areatype]
   
-  vaccine_type2 = c("percent who received 2+ doses of HPV vaccine, ages 13-17",
-                    "percent who received 3+ doses of HPV vaccine, ages 13-17"
-  )
-  
-  if (vaccine %in% vaccine_type1) {
+  if (stage == "all stages") {
     resp %>% 
-      setNames(c("State", "FIPS", "Met Objective of 80.0%?", "Percent", "Lower 95% CI", "Upper 95% CI", "Number of Respondents"))
-  } else {
+      setNames(c(areatype_title, "FIPS", "Age Adjusted Incidence Rate", "Lower 95% CI", "Upper 95% CI", "CI Rank", "Lower CI Rank", "Upper CI Rank", "Annual Average Count", "Recent Trend", "Recent 5 Year Trend", "Trend Lower 95% CI", "Trend Upper 95% CI"))
+  } else if (stage == "late stage (regional & distant)") {
     resp %>% 
-      setNames(c("State", "FIPS", "Percent", "Lower 95% CI", "Upper 95% CI", "Number of Respondents"))
+      setNames(c(areatype_title, "FIPS", "Age Adjusted Incidence Rate", "Lower 95% CI", "Upper 95% CI", "CI Rank", "Lower CI Rank", "Upper CI Rank", "Annual Average Count", "Percentage of Cases with Late Stage"))
   }
 }
---
+++ R/process-incidence.R
@@ -0,0 +1,50 @@
+#' Process Cancer Incidence Response Data
+#' 
+#' This function processes the Cancer Incidence response data from State Cancer Profiles
+#'
+#' @param resp A response object
+#' 
+#' @importFrom httr2 resp_body_string
+#' @importFrom dplyr mutate_all na_if filter mutate
+#' @importFrom rlang sym
+#' @importFrom utils read.csv data
+#' 
+#' @returns A processed response data frame
+#' 
+#' @examples
+#' \dontrun{
+#' process_incidence(resp)
+#' }
+process_incidence <- function(resp) {
+  
+  nenv <- new.env()
+  data("state", envir = nenv)
+  state.name <- nenv$state.name
+  
+  resp_lines <- resp %>%
+    resp_body_string() %>% 
+    strsplit("\\n") %>%  unlist() 
+  
+  index_first_line_break <- which(resp_lines == "")[4]
+  index_second_line_break <- which(resp_lines == "")[5]
+  
+  resp <- resp_lines[(index_first_line_break + 1):(index_second_line_break -1)] %>% 
+    paste(collapse = "\n") %>% 
+    (\(x) read.csv(textConnection(x), header=TRUE, colClasses = "character"))()
+  
+  column <- c("Health.Service.Area", "County", "State")[c("Health.Service.Area", "County", "State") %in% colnames(resp)]
+  
+  resp <- resp %>% 
+    filter(!!sym(column) != "US (SEER+NPCR)(1)")
+  
+  resp <- resp %>%   
+    mutate_all(\(x) na_if(x, "N/A")) %>% 
+    mutate_all(\(x) na_if(x, "data not available")) %>%
+    mutate_all(\(x) na_if(x, "*")) %>% 
+    mutate(!!column := sub("\\(\\d+\\)", "", !!sym(column)))
+  
+  if(column %in% c("Health.Service.Area", "County")) {
+    resp %>%
+      filter(!(!!sym(column) %in% state.name))
+  }
+}
