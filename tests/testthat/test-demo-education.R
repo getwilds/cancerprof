e75c4cc82ed4238f5ee4493772d1fd86984a21ff
realbp <realbp@uw.edu>
2024-02-12 10:32:35 -0800

    edited education tests

--- tests/testthat/test-demo-education.R
+++ tests/testthat/test-demo-education.R
@@ -6,26 +6,19 @@
 test_that("Output data type is correct", {
   output <- demo_crowding("usa", "state", "All Races (includes Hispanic)")
   
-  expect_equal(class(output), "data.frame",
-               info = "Output should be a data frame")
-  
-  expect_equal(typeof(output), "list",
-               info = "Output should have list storage type (since data frames are lists)")
+  expect_true(inherits(output, "data.frame"))
 })
 
 #Ensures that variables are present and working on SCP
 test_that("demo-education returns non-empty data frame", {
   education1 <- demo_education("wa", "county", "at least high school", "males")
-  expect_true(!is.null(education1))
   expect_true(is.data.frame(education1))
   
   education2 <- demo_education("ca", "hsa", "less than 9th grade")
-  expect_true(!is.null(education2))
   expect_true(is.data.frame(education2))
   
   education3 <- demo_education("usa", "state", "at least bachelors degree", 
                                "both sexes", "all races (includes hispanic)")
-  expect_true(!is.null(education3))
   expect_true(is.data.frame(education3))
 })
 
@@ -37,6 +30,8 @@ test_that("demo-education has correct number of columns", {
 })
 
 #test error handling
+
+#write one for error handling in education - mention in the message how values could have chnaged in SCP
 test_that("demo-education handles invalid education parameters", {
   expect_error(
     demo_education("wa", "county", "less than 9th grade", "males"),
@@ -51,9 +46,3 @@ test_that("demo-education handles invalid education parameters", {
     "For At Least Bachelors Degree, Race and Sex must be NOT NULL."
   )
 })
-
-# What expected data would I be comparing too?
-
-# test_that("demo-education returns the expected columns", {
-#   expect_identical(names(education2), names(expected_data))
-# })
